a7482f56c19bb8b7b4bc31d3328d589d1b76d4d4
DanielOchoa1214 <77862016+DanielOchoa1214@users.noreply.github.com>
2024-12-02 15:19:36 -0500

    My first GO app, lets goooo

---
+++ middlewares/auth.go
@@ -0,0 +1,25 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"events.com/rest-api/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized! GET OUT"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return 
+	}
+
+	context.Set("userId", userId)
+	context.Next()
+}
---
+++ routes/users.go
@@ -0,0 +1,48 @@
+package routes
+
+import (
+	"net/http"
+
+	"events.com/rest-api/models"
+	"events.com/rest-api/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func signUp(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the request data :("})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user"})
+	}
+	context.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
+}
+
+func login(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the request data :("})
+		return
+	}
+
+	err = user.ValidateCredentials()
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
+	token, err := utils.GenerateToken(user.Email, user.ID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could note authenticate user! D:"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful! :D", "token": token})
+}
